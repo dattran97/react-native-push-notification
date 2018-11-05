2cca71d985aeeae6a6d527b3456e17cf6d7a5dd4
dattran97 <tuandat1079@gmail.com>
2018-11-05 11:42:38 +0700

    Update RNPushNotificationHelper.java
    Changed setContentText key from 'message' to 'alert'

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -83,8 +83,8 @@ public class RNPushNotificationHelper {
             return;
         }
 
-        if (bundle.getString("message") == null) {
-            Log.e(LOG_TAG, "No message specified for the scheduled notification");
+        if (bundle.getString("alert") == null) {
+            Log.e(LOG_TAG, "No alert specified for the scheduled notification");
             return;
         }
 
@@ -140,9 +140,9 @@ public class RNPushNotificationHelper {
                 return;
             }
 
-            if (bundle.getString("message") == null) {
+            if (bundle.getString("alert") == null) {
                 // this happens when a 'data' notification is received - we do not synthesize a local notification in this case
-                Log.d(LOG_TAG, "Cannot send to notification centre because there is no 'message' field in: " + bundle);
+                Log.d(LOG_TAG, "Cannot send to notification centre because there is no 'alert' field in: " + bundle);
                 return;
             }
 
@@ -217,7 +217,7 @@ public class RNPushNotificationHelper {
                 notification.setGroup(group);
             }
 
-            notification.setContentText(bundle.getString("message"));
+            notification.setContentText(bundle.getString("alert"));
 
             String largeIcon = bundle.getString("largeIcon");
 
@@ -267,7 +267,7 @@ public class RNPushNotificationHelper {
             String bigText = bundle.getString("bigText");
 
             if (bigText == null) {
-                bigText = bundle.getString("message");
+                bigText = bundle.getString("alert");
             }
 
             notification.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
